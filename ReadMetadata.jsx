610d5b516fcca9f8353817845e0b415ea0823d24
Arturo Lecumberri <arturolecumberri@outlook.com>
2022-08-30 05:44:48 +0700

    Images included next to metadata info

--- ReadMetadata.jsx
+++ ReadMetadata.jsx
@@ -10,6 +10,38 @@ function getExifTagIndex(exifData, exifTag) {
   }
 }
 
+function MoveLayerTo(fLayer,fX,fY, anchorPosition) {
+
+  var Position = fLayer.bounds;
+  var Width = fLayer.bounds[2].value - fLayer.bounds[0].value;
+  var Height = fLayer.bounds[3].value - fLayer.bounds[1].value;
+
+  switch (anchorPosition) {
+    case "middlecenter":
+    Position[0] = fX - Position[0] + Width / 2;
+    Position[1] = fY - Position[1] + Height / 2;
+    break;
+
+    case "middleleft":
+    Position[0] = fX - Position[0]
+    Position[1] = fY - Position[1] + Height / 2;
+    break;
+
+    case "middleright":
+    Position[0] = fX - Position[0]
+    Position[1] = fY - Position[1] - Height / 2;
+    break;
+
+    default:
+    Position[0] = fX - Position[0]
+    Position[1] = fY - Position[1]
+
+  }
+  
+
+  fLayer.translate(-Position[0],-Position[1]);
+}
+
 function toTitleCase(str) {
   return str.replace(
     /\w\S*/g,
@@ -26,37 +58,47 @@ function nameFile() {
 }
 
 // Based on https://www.codeproject.com/Questions/882480/Place-Embedded-through-photoshop-scripting-Javascr
-function addImage (selectedFilePath, xPosition, yPosition) {
-    var idPlc = charIDToTypeID( "Plc " ); 
-    var desc11 = new ActionDescriptor();  
-    var idnull = charIDToTypeID( "null" );
-    
-    // opens dialog to select file
-    // var selectedFile = app.openDialog();
+function addImage (selectedFilePath, targetGroupName, targetWidth) {
+  
+  var idPlc = charIDToTypeID( "Plc " ); 
+  var desc11 = new ActionDescriptor();  
+  var idnull = charIDToTypeID( "null" );
+  
+  // opens dialog to select file
+  // var selectedFile = app.openDialog();
 
-    desc11.putPath( idnull, new File(selectedFilePath) );
-    var idFTcs = charIDToTypeID( "FTcs" ); 
-    var idQCSt = charIDToTypeID( "QCSt" );   
-    var idQcsa = charIDToTypeID( "Qcsa" ); 
-    desc11.putEnumerated( idFTcs, idQCSt, idQcsa );
-    var idOfst = charIDToTypeID( "Ofst" );     
-    var desc12 = new ActionDescriptor();     
-    var idHrzn = charIDToTypeID( "Hrzn" );    
-    var idPxl = charIDToTypeID( "#Pxl" );      
-    desc12.putUnitDouble( idHrzn, idPxl, 0.000000 );     
-    var idVrtc = charIDToTypeID( "Vrtc" );    
-    var idPxl = charIDToTypeID( "#Pxl" );    
-    desc12.putUnitDouble( idVrtc, idPxl, 0.000000 );
-    var idOfst = charIDToTypeID( "Ofst" );
-    desc11.putObject( idOfst, idOfst, desc12 );
-    executeAction( idPlc, desc11, DialogModes.NO );
+  desc11.putPath( idnull, new File(selectedFilePath) );
+  var idFTcs = charIDToTypeID( "FTcs" ); 
+  var idQCSt = charIDToTypeID( "QCSt" );   
+  var idQcsa = charIDToTypeID( "Qcsa" ); 
+  desc11.putEnumerated( idFTcs, idQCSt, idQcsa );
+  var idOfst = charIDToTypeID( "Ofst" );     
+  var desc12 = new ActionDescriptor();     
+  var idHrzn = charIDToTypeID( "Hrzn" );    
+  var idPxl = charIDToTypeID( "#Pxl" );      
+  desc12.putUnitDouble( idHrzn, idPxl, 0.000000 );     
+  var idVrtc = charIDToTypeID( "Vrtc" );    
+  var idPxl = charIDToTypeID( "#Pxl" );    
+  desc12.putUnitDouble( idVrtc, idPxl, 0.000000 );
+  var idOfst = charIDToTypeID( "Ofst" );
+  desc11.putObject( idOfst, idOfst, desc12 );
+  executeAction( idPlc, desc11, DialogModes.NO );
 
-    var doc = activeDocument;
-    // TODO: Posicionar la imagen
+  // Document Selection
+  var doc = activeDocument;
+
+  // Resize image
+  var imageWidth = doc.activeLayer.bounds[2].value - app.activeDocument.activeLayer.bounds[0].value;
+  var resizeRatio = targetWidth / imageWidth * 180;
+  doc.activeLayer.resize(resizeRatio, resizeRatio, AnchorPosition.MIDDLECENTER);
+
+  // Move inside group
+  doc.activeLayer.move(doc.layerSets.getByName(targetGroupName), ElementPlacement.INSIDE);
+
     
 }
 
-function addMetadataAsText (exifTag, targetGroupName, colorHexValue, fontName, fontSize, xPosition, yPosition) {
+function addMetadataAsText (exifTag, targetGroupName, colorHexValue, fontName, fontSizePixels) {
 
     // Document selection
     var doc = activeDocument;
@@ -71,9 +113,6 @@ function addMetadataAsText (exifTag, targetGroupName, colorHexValue, fontName, f
     // Text 
     var textItemRef = txtLayer.textItem;
 
-    // Text position definition
-    textItemRef.position = [xPosition, yPosition];
-
     // Text color
     textColor = new SolidColor();
     textColor.rgb.hexValue = colorHexValue;
@@ -82,8 +121,9 @@ function addMetadataAsText (exifTag, targetGroupName, colorHexValue, fontName, f
     // Text font
     textItemRef.font = fontName;
 
-    // Text size TODO: Find a way to define size based on canvas height
-    textItemRef.size=fontSize;
+    // Font size. There is a bug. textItem.size always converts "px" to "pt". 
+    // https://community.adobe.com/t5/photoshop-ecosystem-discussions/photoshop-script-change-textitem-size-javascript/td-p/11478075
+    textItemRef.size = new UnitValue(fontSizePixels * 72 / doc.resolution, 'pt');;
 
     // Finding the exif entry index (Variable and Value) for the desired exifTag 
     // as defined in https://web.archive.org/web/20190624045241if_/http://www.cipa.jp:80/std/documents/e/DC-008-Translation-2019-E.pdf
@@ -95,106 +135,165 @@ function addMetadataAsText (exifTag, targetGroupName, colorHexValue, fontName, f
         textItemRef.contents = "ISO " + doc.info.exif[exifTagIndex][1];
         break;
 
-        case 272:
-        var textString = doc.info.exif[exifTagIndex][1];
-        textItemRef.contents = toTitleCase(textString);
-        break;
-
         default:
         textItemRef.contents = doc.info.exif[exifTagIndex][1];
     }
 
     // Text center
     //textItemRef.kind = TextType.PARAGRAPHTEXT;
-    //var verticalCenterOffset = textItemRef.height / 2;
+    //var YCenterOffset = textItemRef.height / 2;
     //var textCenterOffset = textItemRef.size / 2;
-    //textItemRef.baselineShift = textCenterOffset - verticalCenterOffset;
+    //textItemRef.baselineShift = textCenterOffset - YCenterOffset;
 
 }
 
 function addMetadataVerticallyDistributed (exifTagsArray, targetGroupName) {
-    
-    // Document size
-    var doc = activeDocument;
-    var docHeight = doc.height.value;
-    var docWidth = doc.width.value;
 
-    // Document Header and Footer Margins
-    var docHeightMarginsAsPercentage = 0.3; // Defines percentage of margins, computed as (amount of pixels in header + footer) / document height. TODO: Define automatically
-    var docHeightMarginsAsPixels = docHeightMarginsAsPercentage * docHeight;
+  // Save current preferences
+  var startRulerUnits = app.preferences.rulerUnits;
+  var startTypeUnits = app.preferences.typeUnits;
+  var startTypeDialogs = app.displayDialogs;
 
-    // Document Content
-    var rowsHeightAsPercentageOfContent = 0.7; // Defines percentage of content, computed as (sum of rows height) / (sum of rows height + sum of rows indents height). TODO: Define automatically
-    var docHeightContentAsPercentage = 1 - docHeightMarginsAsPercentage;
+  // Set own preferences
+  app.preferences.rulerUnits = Units.PIXELS;
+  app.preferences.typeUnits = TypeUnits.PIXELS;
+  app.displayDialogs = DialogModes.NO;
 
-    // Number of rows 
-    var exifItemsCount = exifTagsArray.length;
+  // Document size
+  var doc = activeDocument;
+  var docHeight = doc.height.value;
+  var docWidth = doc.width.value;
 
-    // Rows Height calculation. Computed based on number of entries
-    var rowsHeightAsPercentage = (1 - 2 * docHeightMarginsAsPercentage) * rowsHeightAsPercentageOfContent / exifItemsCount;
-    var rowsHeightAsPixels = rowsHeightAsPercentage * docHeight;
+  // Document Header and Footer Margins
+  var docMarginsHeightProportion = 0.6; // Defines percentage of margins, computed as (amount of pixels in header + footer) / document height. TODO: Define automatically
+  var docContentHeightProportion = 1 - docMarginsHeightProportion;
 
-    // Rows Indents calculation
-    var rowIndentsHeightAsPercentage = (1 - 2 * docHeightMarginsAsPercentage) * (1 - rowsHeightAsPercentageOfContent) / (exifItemsCount - 1);
-    var rowIndentsHeightAsPixels = rowIndentsHeightAsPercentage * docHeight;
-    
-    // Font size based on canvas size
-    var fontSize = new UnitValue(rowsHeightAsPixels/10, "px");
+  // Document Content
+  var rowsProportion = 0.5; // Defines percentage of content, computed as (sum of rows height) / (sum of rows height + sum of rows indents height). TODO: Define automatically
+  var rowIndentsProportion = 1 - rowsProportion;
 
-    // Initial vertical position
-    var exifItemInitialVerticalPosition = docHeightMarginsAsPixels + (rowsHeightAsPixels + rowIndentsHeightAsPixels) / 2;
-    var exifItemVerticalPosition = exifItemInitialVerticalPosition;
+  // Number of rows 
+  var exifItemsCount = exifTagsArray.length;
 
-    // Horizontal position - Static
-    var exifItemHorizontalPosition = 0.5 * docWidth;
+  // Rows Height calculation. Computed based on number of entries
+  var rowsHeight = docContentHeightProportion * rowsProportion / exifItemsCount;
+  var rowsHeightPixels = rowsHeight * docHeight; // Used as fontSizePixels for addMetadataAsText
 
-    // Group definition for the group
-    var group = app.activeDocument.layerSets.add();
-    group.name = targetGroupName;
+  // Rows Indents calculation
+  var rowIndentsHeight = docContentHeightProportion * rowIndentsProportion / (exifItemsCount - 1);
+  var rowIndentsHeightPixels = rowIndentsHeight * docHeight;
 
-    for (var i=0; i<exifTags.length; i++) {
-        addMetadataAsText(exifTagsArray[i], targetGroupName, "FFFFFF", "Comfortaa-Bold", fontSize, exifItemHorizontalPosition, exifItemVerticalPosition);
-        exifItemVerticalPosition = exifItemVerticalPosition + rowIndentsHeightAsPixels + rowsHeightAsPixels;
-        // Guardar el id en un array para seleccionar todas las capas, agruparlas y alinearlas
-        // doc.activeLayer.id
-    };
+  // Initial Y position
+  var exifItemInitialYPosition = (docMarginsHeightProportion / 2  + rowsHeight / 2) * docHeight;
+  var exifItemYPosition = exifItemInitialYPosition;
 
-    centerLayerGroupVertically(targetGroupName);
+  // X position - Static
+  var exifItemXPosition = 0.5 * docWidth;
+
+  // Group definition for the group
+  var group = app.activeDocument.layerSets.add();
+  group.name = targetGroupName;
+
+  for (var i=0; i<exifTagsArray.length; i++) {
+      addMetadataAsText(exifTagsArray[i], targetGroupName, "FFFFFF", "Comfortaa-Bold", rowsHeightPixels, exifItemXPosition, exifItemYPosition);
+      exifItemYPosition = exifItemYPosition + rowIndentsHeightPixels + rowsHeightPixels;
+      // Guardar el id en un array para seleccionar todas las capas, agruparlas y alinearlas
+      // doc.activeLayer.id
+  };
+
+  // Reset application preferences
+  app.preferences.rulerUnits = startRulerUnits;
+  app.preferences.typeUnits = startTypeUnits;
+  app.displayDialogs = startTypeDialogs;
 
 }
 
 
-function addImageAndMetadata(exifTag) {
+function addImageAndMetadata(exifTagsArray) {
 
-    // Document selection
-    var doc = activeDocument;
+  // Save current preferences
+  var startRulerUnits = app.preferences.rulerUnits;
+  var startTypeUnits = app.preferences.typeUnits;
+  var startTypeDialogs = app.displayDialogs;
 
-    // Group selection
-    var layerGroup = doc.layerSets.getByName(exifTag);
+  // Set own preferences
+  app.preferences.rulerUnits = Units.PIXELS;
+  app.preferences.typeUnits = TypeUnits.PIXELS;
+  app.displayDialogs = DialogModes.ERROR;
 
-    addMetadataAsText(exifTag)
+  // Document size
+  var doc = activeDocument;
+  var docHeight = doc.height.value;
+  var docWidth = doc.width.value;
 
-    
+  // Document Header and Footer Margins
+  var docMarginsHeightProportion = 0.5; // Defines percentage of margins, computed as (amount of pixels in header + footer) / document height. TODO: Define automatically
+  var docContentHeightProportion = 1 - docMarginsHeightProportion;
 
+  // Document Content
+  var rowsProportion = 0.35; // Defines percentage of content, computed as (sum of rows height) / (sum of rows height + sum of rows indents height). TODO: Define automatically
+  var rowIndentsProportion = 1 - rowsProportion;
+
+  // Number of rows 
+  var exifItemsCount = exifTagsArray.length;
+
+  // Rows Height calculation. Computed based on number of entries
+  var rowsHeight = docContentHeightProportion * rowsProportion / exifItemsCount;
+  var rowsHeightPixels = rowsHeight * docHeight; // Used as fontSizePixels for addMetadataAsText and as targetWidth for addImage
+
+  // Rows Indents calculation
+  var rowIndentsHeight = docContentHeightProportion * rowIndentsProportion / (exifItemsCount - 1);
+  var rowIndentsHeightPixels = rowIndentsHeight * docHeight;
+
+  // Initial Y position
+  var exifItemInitialYPosition = (docMarginsHeightProportion / 2  + rowsHeight / 2) * docHeight;
+  var exifItemYPosition = exifItemInitialYPosition;
+
+  // X position - Static
+  var imageXPosition = 0.4 * docWidth;
+  var exifDataXPosition = 0.5 * docWidth;
+
+  for (var i=0; i<exifTagsArray.length; i++) {
+
+    // Group for both the image and the text
+    var group = app.activeDocument.layerSets.add();
+    group.name = exifTagsArray[i];
 
     // Be careful with the encoding for the "Ó": http://www.javascripter.net/faq/accentedcharacters.htm
-    var selectedFilePath = "D:/OneDrive/Arturo - Personal/\xD3liver Lalan/Instagram Photos/Assets/Icons/" + exifTag + ".svg";
-    addImage (selectedFilePath);
+    var selectedFilePath = "D:/OneDrive/Arturo - Personal/\xD3liver Lalan/Instagram Photos/Assets/Icons/" + exifTagsArray[i] + ".svg";
+    addImage (selectedFilePath, exifTagsArray[i], rowsHeightPixels)
+
+    MoveLayerTo(doc.activeLayer, imageXPosition, exifItemYPosition, "middlecenter");
+
+    addMetadataAsText(exifTagsArray[i], exifTagsArray[i], "FFFFFF", "Comfortaa-Bold", rowsHeightPixels);
+
+    MoveLayerTo(doc.activeLayer, exifDataXPosition, exifItemYPosition, "middleleft");
+
+    // Next Y position
+    exifItemYPosition = exifItemYPosition + rowIndentsHeightPixels + rowsHeightPixels;
+
+  };
+
+  // Reset application preferences
+  app.preferences.rulerUnits = startRulerUnits;
+  app.preferences.typeUnits = startTypeUnits;
+  app.displayDialogs = startTypeDialogs;
+
 }
 
 
 // Based on https://morris-photographics.com/photoshop/scripts/downloads/Center%20Layer%201-1-0.jsx
-function centerLayerGroupVertically(layerGroupName) {
+function centerLayerGroupYly(layerGroupName) {
 
-    // get document attributes
-    var doc = activeDocument;
-    var docHeight = doc.height.value;
+  // get document attributes
+  var doc = activeDocument;
+  var docHeight = doc.height.value;
 
-    // get layer group
-    var layer = doc.layerSets.getByName("exifData");
-    var bounds = layer.bounds;
+  // get layer group
+  var layer = doc.layerSets.getByName("exifData");
+  var bounds = layer.bounds;
 
-    // get layer dimensions
+  // get layer dimensions
 	var layerHeight = Number(bounds[3] - bounds[1]);
 
 	// calculate offsets
@@ -205,9 +304,7 @@ function centerLayerGroupVertically(layerGroupName) {
 }
 
 
-function makeDarkerNoisierBlurier(targetLayer) {
-
-    // Document selection
+function makeDarkerNoisierBlurier(targetLayer) {// Document selection
     var doc = activeDocument;
 
     // Layer selection
@@ -262,6 +359,9 @@ function makeDarkerNoisierBlurier(targetLayer) {
 nameFile();
 
 makeDarkerNoisierBlurier("photoParameters");
+ 
+//addMetadataVerticallyDistributed([37377, 37378, 34855, 37386], "exifData");
 
-var exifTags = [37377, 37378, 34855, 37386]
-addMetadataVerticallyDistributed(exifTags, "exifData");
+addImageAndMetadata([37377, 37378, 34855, 37386]);
+
+//centerLayerGroupYly("exifData");
