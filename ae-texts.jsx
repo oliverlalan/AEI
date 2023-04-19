be3af8096c860c294f4b2a283ccc0d071f4e94e4
Arturo Lecumberri <arturolecumberri@outlook.com>
2023-04-19 14:10:21 +0200

    createGroupTitleLayer function added createPanelTitleLayer function added

--- ae-texts.jsx
+++ ae-texts.jsx
@@ -36,7 +36,7 @@ function createSliderTextLabelLayer (targetComposition, setting, position, ancho
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Description: 
-// Call: createAnimatedTextLayer (0, 100, 2, 4, [0,0],...)
+// Call: createSliderTextValueLayer (sliderComposition, [setting.defaultValue, setting.settingValue], [sliderTextParameters.animation.start, sliderTextParameters.animation.end], sliderTextValueParameters.position, sliderTextValueParameters.anchorPoint, sliderTextValueParameters.justification)
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function createSliderTextValueLayer (targetComposition, valuesInterval, timesInterval, position, anchorPoint, justification) {
@@ -67,5 +67,76 @@ function createSliderTextValueLayer (targetComposition, valuesInterval, timesInt
 
     // Select the text layer
     textLayer.selected = true;
+
+    return textLayer;
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Description: 
+// Call: createGroupTitleLayer (groupComposition, groupName)
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function createGroupTitleLayer (targetComposition, text)  {
+
+    // Create a new text layer
+    var textLayer = targetComposition.layers.addText(text.toString().toUpperCase());
+    textLayer.name = "Group Title";
+
+    // Set the font properties
+    var textProperties = textLayer.property("Source Text").value;
+    textProperties.resetCharStyle();
+    textProperties.fontSize = groupTitleParameters.fontSize;
+    textProperties.fillColor = groupTitleParameters.fontColor;
+    textProperties.font = groupTitleParameters.fontName;
+    textProperties.tracking = groupTitleParameters.fontTracking;
+
+    // Set justification
+    textProperties.justification = groupTitleParameters.justification;
+
+    // Set the text properties back to the layer
+    textLayer.property("Source Text").setValue(textProperties);
+
+    // Set the position and anchor point
+    setAnchorPoint(textLayer, groupTitleParameters.anchorPoint);
+    textLayer.position.setValue(groupTitleParameters.position);
+
+    groupCompositionParameters.position[1] += groupTitleParameters.height;
+
+    return textLayer;
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Description: 
+// Call: createAnimatedTextLayer (0, 100, 2, 4, [0,0],...)
+// TODO: Define function: createPanelTitleComposition for consistency purposes
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function createPanelTitleLayer (targetComposition, text)  {
+
+    // Create a new text layer
+    var textLayer = targetComposition.layers.addText(text.toString().toUpperCase());
+    textLayer.name = "Panel Title";
+
+    // Set the font properties
+    var textProperties = textLayer.property("Source Text").value;
+    textProperties.resetCharStyle();
+    textProperties.fontSize = panelTitleParameters.fontSize;
+    textProperties.fillColor = panelTitleParameters.fontColor;
+    textProperties.font = panelTitleParameters.fontName;
+    textProperties.tracking = panelTitleParameters.fontTracking;
+
+    // Set justification
+    textProperties.justification = panelTitleParameters.justification;
+
+    // Set the text properties back to the layer
+    textLayer.property("Source Text").setValue(textProperties);
+
+    // Set the position and anchor point
+    setAnchorPoint(textLayer, panelTitleParameters.anchorPoint);
+    textLayer.position.setValue(panelTitleParameters.position);
+
+    return textLayer;
 
 }
