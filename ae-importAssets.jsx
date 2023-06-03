fe365ddfb75d67d10d7c78e2679361ecd9593f30
Arturo Lecumberri <arturolecumberri@outlook.com>
2023-06-03 13:34:11 +0300

    Library to import assets like ffx files and png

---
+++ ae-importAssets.jsx
@@ -0,0 +1,2 @@
+// TODO Define a function to import all items in a given folder and store them in an object named "assets" with the following properties: "Name": The name of the file without extension
+
